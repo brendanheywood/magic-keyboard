9b24b86d27c8e8fbf10bf195f2aaa31ce3ced4fa
Brendan Heywood <brendan@catalyst-au.net>
2013-09-15 22:10:38 +1000

    Compressed html in help dialog

--- js/magic-keyboard.js
+++ js/magic-keyboard.js
@@ -9,20 +9,10 @@
 
     mk.showHelp = function(){
         if (!mk.helpEl){
-            mk.helpEl = $('\
-<div class="modal fade" id="magic-kb-help" tabindex="-1" role="dialog" aria-labelledby="myModalLabel" aria-hidden="true">\
-    <div class="modal-dialog">\
-      <div class="modal-content">\
-        <div class="modal-header">\
-          <button type="button" class="close" data-dismiss="modal" aria-hidden="true">&times;</button>\
-          <h4 class="modal-title">Keyboard Shortcuts <kbd>?</kbd></h4>\
-        </div>\
-        <div class="modal-body">\
-        </div>\
-      </div><!-- /.modal-content -->\
-    </div><!-- /.modal-dialog -->\
-  </div><!-- /.modal -->\
-');
+            mk.helpEl = $('<div class="modal fade" id="magic-kb-help" tabindex="-1"><div class="modal-dialog"><div class="modal-content"><div class="modal-header">\
+<button type=button class=close data-dismiss=modal aria-hidden=true>&times;</button>\
+<h4 class="modal-title">Keyboard Shortcuts <kbd>?</kbd></h4>\
+</div><div class="modal-body"></div></div></div></div>');
             var help = '';
             $.each(mk.shortcuts, function(i,e){
                 help += '<tr>'
